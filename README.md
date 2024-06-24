c958d13b8cf29c60ba22aea838fdd5f3dcd4ce84
BitByLogic <33465177+BitByLogics@users.noreply.github.com>
2024-06-24 09:07:57 -0400

    Small overhaul!

---
+++ README.md
@@ -0,0 +1,12 @@
+<div align="center">
+<img src="https://www.spigotmc.org/data/avatars/l/1210/1210251.jpg?1687520101">
+<h1>One Player Sleep</h1>
+</div>
+
+## What is OnePlayerSleep?
+This plugin enables a Minecraft server feature where only one player needs to sleep in a bed to transition from night to day, eliminating the need for all players to sleep simultaneously to skip the night.
+
+## BStats
+![BStats Graph](https://bstats.org/signatures/bukkit/OnePlayerSleepPlugin.svg)
+
+# Download from [Spigot](https://www.spigotmc.org/resources/oneplayersleep-1-16-1-21.115383/)!
--- src/main/java/com/ashkiano/oneplayersleep/OnePlayerSleep.java
+++ src/main/java/com/ashkiano/oneplayersleep/OnePlayerSleep.java
@@ -1,60 +1,85 @@
 package com.ashkiano.oneplayersleep;
 
+import com.jeff_media.updatechecker.UpdateCheckSource;
+import com.jeff_media.updatechecker.UpdateChecker;
+import com.jeff_media.updatechecker.UserAgentBuilder;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.permissions.ServerOperator;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.List;
 
-public class OnePlayerSleep extends JavaPlugin {
+public class OnePlayerSleep extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new SleepListener(this), this);
-        Metrics metrics = new Metrics(this, 21209);
-        this.getLogger().info("Thank you for using the OnePlayerSleep plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
-        checkForUpdates();
-    }
+        getServer().getPluginManager().registerEvents(this, this);
 
-    private void checkForUpdates() {
-        try {
-            String pluginName = this.getDescription().getName();
-            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
+        new Metrics(this, 21209);
 
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+        new UpdateChecker(this, UpdateCheckSource.SPIGOT, "115383")
+                .setNotifyRequesters(false)
+                .setNotifyOpsOnJoin(false)
+                .setUserAgent(UserAgentBuilder.getDefaultUserAgent())
+                .checkEveryXHours(12)
+                .onSuccess((commandSenders, latestVersion) -> {
+                    String messagePrefix = "&8[&6One Player Sleep&8] ";
+                    String currentVersion = getDescription().getVersion();
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
+                    if (currentVersion.equalsIgnoreCase(latestVersion)) {
+                        String updateMessage = color(messagePrefix + "&aYou are using the latest version of OnePlayerSleep!");
 
-                String jsonResponse = response.toString();
-                JSONObject jsonObject = new JSONObject(jsonResponse);
-                if (jsonObject.has("error")) {
-                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
-                } else {
-                    String latestVersion = jsonObject.getString("latest_version");
-
-                    String currentVersion = this.getDescription().getVersion();
-                    if (currentVersion.equals(latestVersion)) {
-                        this.getLogger().info("This plugin is up to date!");
-                    } else {
-                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                        Bukkit.getConsoleSender().sendMessage(updateMessage);
+                        Bukkit.getOnlinePlayers().stream().filter(ServerOperator::isOp).forEach(player -> player.sendMessage(updateMessage));
+                        return;
                     }
-                }
-            } else {
-                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
-            }
-        } catch (Exception e) {
-            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
-        }
+
+                    List<String> updateMessages = List.of(
+                            color(messagePrefix + "&cYour version of OnePlayerSleep is outdated!"),
+                            color(String.format(messagePrefix + "&cYou are using %s, latest is %s!", currentVersion, latestVersion)),
+                            color(messagePrefix + "&cDownload latest here:"),
+                            color("&6https://www.spigotmc.org/resources/oneplayersleep-1-16-1-21.115383/")
+                    );
+
+                    Bukkit.getConsoleSender().sendMessage(updateMessages.toArray(new String[]{}));
+                    Bukkit.getOnlinePlayers().stream().filter(ServerOperator::isOp).forEach(player -> player.sendMessage(updateMessages.toArray(new String[]{})));
+                })
+                .onFail((commandSenders, e) -> {
+                }).checkNow();
+
+        // Print the donation message to the console
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            Bukkit.getConsoleSender().sendMessage(
+                    ChatColor.GOLD + "Thank you for using the OnePlayerSleep plugin!",
+                    ChatColor.GOLD + "If you enjoy using this plugin!",
+                    ChatColor.GOLD + "Please consider making a donation to support the development!",
+                    ChatColor.GOLD + "You can donate at: " + ChatColor.GREEN + "https://donate.ashkiano.com"
+            );
+        }, 20);
     }
+
+    private String color(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    @EventHandler
+    public void onPlayerBedEnter(PlayerBedEnterEvent event) {
+        if (event.getBedEnterResult() != PlayerBedEnterEvent.BedEnterResult.OK) {
+            return;
+        }
+
+        getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
+            World world = event.getPlayer().getWorld();
+            world.setTime(0); // Sets the time to day.
+            world.setStorm(false); // Clears the weather.
+            world.setThundering(false); // Stops thundering.
+        }, 100L); // Delay in server ticks before running the task, to ensure it happens after the player is in bed.
+    }
+
 }
