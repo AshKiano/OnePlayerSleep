1f58ac9dbed607f423854b4f7b18a7978874d0db
jvyskocil <ash.kiano@gmail.com>
2024-07-09 16:27:49 +0200

    version 1.5

--- src/main/java/com/ashkiano/oneplayersleep/OnePlayerSleep.java
+++ src/main/java/com/ashkiano/oneplayersleep/OnePlayerSleep.java
@@ -1,8 +1,5 @@
 package com.ashkiano.oneplayersleep;
 
-import com.jeff_media.updatechecker.UpdateCheckSource;
-import com.jeff_media.updatechecker.UpdateChecker;
-import com.jeff_media.updatechecker.UserAgentBuilder;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,10 +7,13 @@ import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.permissions.ServerOperator;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class OnePlayerSleep extends JavaPlugin implements Listener {
 
@@ -22,36 +22,6 @@ public class OnePlayerSleep extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         new Metrics(this, 21209);
-
-        new UpdateChecker(this, UpdateCheckSource.SPIGOT, "115383")
-                .setNotifyRequesters(false)
-                .setNotifyOpsOnJoin(false)
-                .setUserAgent(UserAgentBuilder.getDefaultUserAgent())
-                .checkEveryXHours(12)
-                .onSuccess((commandSenders, latestVersion) -> {
-                    String messagePrefix = "&8[&6One Player Sleep&8] ";
-                    String currentVersion = getDescription().getVersion();
-
-                    if (currentVersion.equalsIgnoreCase(latestVersion)) {
-                        String updateMessage = color(messagePrefix + "&aYou are using the latest version of OnePlayerSleep!");
-
-                        Bukkit.getConsoleSender().sendMessage(updateMessage);
-                        Bukkit.getOnlinePlayers().stream().filter(ServerOperator::isOp).forEach(player -> player.sendMessage(updateMessage));
-                        return;
-                    }
-
-                    List<String> updateMessages = List.of(
-                            color(messagePrefix + "&cYour version of OnePlayerSleep is outdated!"),
-                            color(String.format(messagePrefix + "&cYou are using %s, latest is %s!", currentVersion, latestVersion)),
-                            color(messagePrefix + "&cDownload latest here:"),
-                            color("&6https://www.spigotmc.org/resources/oneplayersleep-1-16-1-21.115383/")
-                    );
-
-                    Bukkit.getConsoleSender().sendMessage(updateMessages.toArray(new String[]{}));
-                    Bukkit.getOnlinePlayers().stream().filter(ServerOperator::isOp).forEach(player -> player.sendMessage(updateMessages.toArray(new String[]{})));
-                })
-                .onFail((commandSenders, e) -> {
-                }).checkNow();
 
         // Print the donation message to the console
         Bukkit.getScheduler().runTaskLater(this, () -> {
@@ -62,6 +32,8 @@ public class OnePlayerSleep extends JavaPlugin implements Listener {
                     ChatColor.GOLD + "You can donate at: " + ChatColor.GREEN + "https://donate.ashkiano.com"
             );
         }, 20);
+
+        checkForUpdates();
     }
 
     private String color(String message) {
@@ -82,4 +54,43 @@ public class OnePlayerSleep extends JavaPlugin implements Listener {
         }, 100L); // Delay in server ticks before running the task, to ensure it happens after the player is in bed.
     }
 
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://plugins.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
+    }
 }
